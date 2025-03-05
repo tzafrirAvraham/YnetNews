424a5760dcdd5e2e96729a3872d0c83723edd56e
Amitmazor94 <amitma94@gmail.com>
2025-03-05 21:57:41 +0200

    update_05/03/2025

--- test/pageobjects/techChurch.page.js
+++ test/pageobjects/techChurch.page.js
@@ -24,7 +24,7 @@ get twoMainArticlesButton(){ return $$(".hero-package-2__upnext .wp-block-techcr
 get restOfArticlesButton(){ return $$("//div[contains(@class, 'wp-block-group heading')]/following-sibling::div/ul/li");}
 
 
-//----------------------------------------------------------
+    //----------------------------------------------------------
    //Click
    //----------------------------------------------------------
 
@@ -49,24 +49,127 @@ get restOfArticlesButton(){ return $$("//div[contains(@class, 'wp-block-group he
             return await BasePage.getText(this.subTitleText);
         }
 
-        async getTime(){
-                let temp;
-                let dateAndTime
-                let time;
-                startStep('print Time text');       
-                endStep();
-                let status=await this.dateTimeText.isExisting();
-                if (status){
-                  dateAndTime=(await this.dateTimeText).split(" ")
-                 // console.log('Shilo dateTime '+dateAndTime)
-                  time = await BasePage.getText(this.time[0]);
-                  return time;
+       async getTime(){
+               let temp;
+               startStep('print Time text');       
+               endStep();
+               let status=await this.dateTimeText.isExisting();
+               if (status)
+                 {console.log('Shilo dateTime '+status)
+                 let timeAndDate= await BasePage.getText(this.dateTimeText);
+                 let arr= timeAndDate.split("·");
+                 temp= arr[0];
+               }
+              else 
+               {console.log('Shilo dateTime '+status)
+                   temp= '00:00'}
+               return temp;
+           }
+
+           async getDate(){
+                   startStep('print Time text');       
+                   endStep();
+                   let timeAndDate= await BasePage.getText(this.dateTimeText);
+                   let arr= timeAndDate.split("·");
+                   let temp= arr[1];
+                   return temp;
+               }
+            
+
+            async getImg(){
+            
+                    startStep('print img text');       
+                    // let list= await this.imageText;
+                    endStep();
+                    return await BasePage.getAtribute(this.imageText,'src');
                 }
-               else 
-                {console.log('Shilo dateTime '+status)
-                    temp= '00:00'}
-                return temp;
-            }
 
 
-} module.exports= new techChurch();
+            async getSummery(){
+                    startStep('print summery text');       
+                    let list= await this.summaryText;
+                    let summary= "";
+                    for( let i=0; i< list.length; i++ ){
+                       summary+= await BasePage.getText(list[i]);
+                    }
+                    endStep();
+                    return summary;
+                }
+
+    //----------------------------------------------------------
+    //Actions (Data)
+    //----------------------------------------------------------
+           
+            async dataFromMainArticle(arr1,num){
+                    startStep('click on main article');
+                    await this.MainArticleButton.click();
+                    endStep();
+                    await this.printData(arr1,num);
+                }    
+                
+                
+            async dataFromTwoMainArticles(arr1,num){
+                    let list= await this.twoMainArticlesButton;
+                
+                    for(let i=0; i< 2; i++){
+                        startStep(" clicking on article number "+ (i+1));
+                        await BasePage.clickButton(list[i]);
+                        await this.printData(arr1,num);
+                        endStep();
+                        num++;
+                        
+                    }
+                }
+
+                async dataFromRestOfArticles(arr1,num){
+                    let list= await this.restOfArticlesButton;
+                    for(let i=0; i< 10; i++){
+                        startStep(" clicking on article number "+ (i+1));
+                        await BasePage.clickButton(list[i]);
+                        await this.printData(arr1,num);
+                        endStep();
+                        num++;
+                        
+                    }
+                }
+
+                
+                async printData(arr1,num){
+        
+        
+                    startStep("collect the data of the article");
+                    let title1=await this.getTitle();
+                    let subTitle1=await this.getSubTitle();
+                    let time1= await this.getTime();
+                    let date1=await this.getDate();
+                    let img1=await this.getImg();
+                    console.log("img link: " +img1);
+                     if (img1?.length<1)         
+                     {img1='https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/122012/one_0.png?itok=wlgLfw4m'}
+                    let summery1=await this.getSummery();
+                    let author1="TechChurch";
+                    // console.log("title is: " +title1);
+                    // console.log("img link: " +img1);
+                    // console.log("sub title: " +subTitle1);
+                    // console.log("time: " + time1)
+                    // console.log("date: " +date1);
+                    // console.log("summery : " +summery1);
+                    // console.log("-----------------------------------------------------------------------------------------------------------------------------------------------");
+                    // console.log("num "+num);
+                    arr1=[{title:title1, subTitle:subTitle1,time:time1, date:date1, image:img1, summary:summery1,author:author1,count: num}];
+                    endStep();
+                     
+                    startStep('push the data to mongoos database')
+                    await mongoDB.CreateOrUpdate(num,TechChurch,arr1)
+                    endStep();
+                
+                    startStep("back to home page");
+                    await browser.back();
+                    endStep();
+                    await browser.pause(4000);
+            
+                }
+            
+
+
+} module.exports = new techChurch();
---
+++ test/specs/techChurch.e2e.js
@@ -0,0 +1,31 @@
+
+const techChurch= require('../pageobjects/techChurch.page')
+const {startStep, endStep, addStep} = require('@wdio/allure-reporter').default;
+const mongoose = require("mongoose");
+const mongoDB= require('../../mongoConnction/mongoDB');
+const TechChurch = require('../../model/techChurch');
+
+describe('', () => {
+
+    let articleArray;
+        let num=1;
+        before('Connect DB ', async()=>{
+            startStep('connction to mongoDB')
+            await mongoDB.connectDB();
+            endStep();
+            startStep('open one website');
+            await browser.url('https://www.one.co.il/');
+            endStep();
+        })
+    
+
+      it('Main Article Data', async()=>{
+                await techChurch.dataFromMainArticle();
+              //await techChurch.data(articleArray,num,);
+              //num++;
+              
+          }) 
+          
+          
+
+});
